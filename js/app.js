b9023933aad352fcd59011f632834f5c87d394d1
gmyrvold <gemguard@gmail.com>
2021-08-03 15:08:00 -0500

    status variables will now increment when the submit button is clicked

--- js/app.js
+++ js/app.js
@@ -1,12 +1,64 @@
 console.log("Tamagotchi Project 0")
 
 class Tamagotchi {
-    constructor(name, age) {
-        this.name = name
+    constructor() {
+        this.name = ''
         this.hunger = 0
         this.sleepiness = 0
         this.boredom = 0
-        this.age = age
+        this.age = 0
+    } 
+    incrementHunger() {
+        const hungerId = setInterval(() => {
+            const hungerElement = document.querySelector('#hunger')
+            hungerElement.innerText = `Hunger: ${this.hunger}`
+    
+            this.hunger ++
+    
+            if (this.hunger < 0) {
+                clearInterval(hungerId)
+            }
+        
+        },1000)
+    }
+    incrementSleepiness() {
+        const sleepId = setInterval(() => {
+            const sleepElement = document.querySelector('#sleepiness')
+            sleepElement.innerText = `Sleepiness: ${this.sleepiness}`
+    
+            this.sleepiness ++
+    
+            if (this.sleepiness < 0) {
+                clearInterval(sleepId)
+            }
+        
+        },1000)
+    }
+    incrementBoredom() {
+        const boredId = setInterval(() => {
+            const boredElement = document.querySelector('#boredom')
+            boredElement.innerText = `Boredom: ${this.boredom}`
+    
+            this.boredom ++
+    
+            if (this.boredom < 0) {
+                clearInterval(boredId)
+            }
+        
+        },1000)
+    }
+    incrementAge() {
+        const ageId = setInterval(() => {
+            const ageElement = document.querySelector('#age')
+            ageElement.innerText = `Age: ${this.age}`
+    
+            this.age ++
+    
+            if (this.age < 0) {
+                clearInterval(ageId)
+            }
+        
+        },1000)
     }
     feed() {
 
@@ -17,9 +69,20 @@ class Tamagotchi {
     play() {
         
     }
+    startGame(){
+        this.incrementHunger()
+        this.incrementSleepiness()
+        this.incrementBoredom()
+        this.incrementAge()
+    }
 }
+const tamagotchi1 = new Tamagotchi ('')
+console.log(tamagotchi1)
 
-
+const buttonSubmit = document.querySelector('button')
+buttonSubmit.addEventListener('click', () => {
+    tamagotchi1.startGame()
+})
 
 
 
