5b84e56fcaa3ebb0db9018d750c89c7b4979fa68
gmyrvold <gemguard@gmail.com>
2021-08-03 11:23:38 -0500

    name input has been created, as well as Tamagotchi class

--- js/app.js
+++ js/app.js
@@ -1,1 +1,40 @@
 console.log("Tamagotchi Project 0")
+
+class Tamagotchi {
+    constructor(name, age) {
+        this.name = name
+        this.hunger = 10
+        this.sleepiness = 10
+        this.boredom = 10
+        this.age = age
+    }
+    feed() {
+
+    }
+    turnOffLights() {
+
+    }
+    play() {
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
